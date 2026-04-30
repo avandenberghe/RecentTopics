74172c56593f579d131ffbb6af950695ef0dd19f
Andy Vandenberghe <andreasvdberghe@avathar.be>
2026-04-30 19:10:25 +0200

    fix style pbtech for recent topics

--- ext.php
+++ ext.php
@@ -15,7 +15,7 @@ namespace avathar\recenttopicsav;
  */
 class ext extends \phpbb\extension\base
 {
-	const RT_VERSION = '3.0.9';
+	const RT_VERSION = '3.0.10';
 	const MIN_PHP_VERSION = '8.1.0';
 	const MIN_PHPBB_VERSION = '3.3.0';
 
